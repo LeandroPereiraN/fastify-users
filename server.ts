b1c98ec6f493ac61e24cc49b17c5de14f8ae5a4b
Mauricio Segovia <maurisegovia15@gmail.com>
2025-08-31 21:22:44 -0300

    Se esta implementando la libreria de pino-pretty, aun no esta terminado

--- server.ts
+++ server.ts
@@ -6,6 +6,18 @@ const fastify = Fastify({
     logger: true
 })
 
+const loggerOptions = {
+    level: process.env.FASTIFY_LOG_LEVEL || 'trace',
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            translateTime: 'SYS:standard',
+            ignore : 'pid,hostname',
+            colorize: true,
+        },
+    }
+}
+
 fastify.register(swagger)
 fastify.register(userRoutes)
 
